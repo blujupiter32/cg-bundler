8eb0e38343286d79912541b7e6ef50b505a09b07
Daniel Trowbridge <blujupiter32@pm.me>
2020-10-28 16:29:07 +0000

    Get command-line arguments

--- bundler/__main__.py
+++ bundler/__main__.py
@@ -1,1 +1,21 @@
 # Copyright 2020 Daniel Trowbridge
+
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog="cg-bundler", description="Bundles program files recursively."
+)
+parser.add_argument(
+    "main", help="the main script to be included last", metavar="MAIN"
+)
+parser.add_argument(
+    "directories", help="a root directory to bundle", metavar="DIR", nargs="+"
+)
+parser.add_argument(
+    "-s",
+    help='the separator to use (default: "BEGIN CODE")',
+    default="BEGIN CODE",
+    metavar="SEP",
+)
+
+args = parser.parse_args()
